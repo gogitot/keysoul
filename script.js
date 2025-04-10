de617766ec47cf9675398b80b0633a87311243cb
gogitot <rgstrategytime@gmail.com>
2025-04-10 18:37:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-
-(function(){
-  emailjs.init("clj3Fq_ElGewSJd3j");
-
-  const form = document.getElementById("contact-form");
-  form.addEventListener("submit", function(e) {
-    e.preventDefault();
-    emailjs.sendForm("service_fbm7d8s", "template_jzaceu8", this)
-      .then(() => {
-        document.getElementById("form-status").innerText = "Съобщението беше изпратено успешно!";
-        form.reset();
-      }, (error) => {
-        document.getElementById("form-status").innerText = "Грешка при изпращане.";
-        console.error(error);
-      });
-  });
-})();
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert("Съобщението е изпратено успешно!");
+});
